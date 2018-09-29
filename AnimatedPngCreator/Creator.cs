467aab2e28155fab66a1ac5ed7dcf1a48ba5d364
cmk1988 <cmk88@web.de>
2018-09-29 13:54:34 +0200

    some classes added

--- AnimatedPngCreator/Creator.cs
+++ AnimatedPngCreator/Creator.cs
@@ -7,58 +7,9 @@ using System.Linq;
 
 namespace CMK
 {
-    internal class Creator : IDisposable
+    internal class Creator : EngineBase, IDisposable
     {
-        private class CrcCalculator
-        {
-            long[] pTable = new long[256];
-
-            //Für Standard CRC32:
-            //(Wert kann verändert werden)
-            long Poly = 0xEDB88320;
-
-            public CrcCalculator()
-            {
-                long CRC;
-                int i, j;
-
-                for (i = 0; i < 256; i++)
-                {
-                    CRC = i;
-
-                    for (j = 0; j < 8; j++)
-                    {
-                        if ((CRC & 0x1) == 1)
-                        {
-                            CRC = (CRC >> 1) ^ Poly;
-                        }
-                        else
-                        {
-                            CRC = (CRC >> 1);
-                        }
-                    }
-                    pTable[i] = CRC;
-                }
-
-            }
-
-            public uint GetCRC32(byte[] input)
-            {
-                long StreamLength, CRC;
-
-                StreamLength = input.Length;
-
-                CRC = 0xFFFFFFFF;
-                for (int i = 0; i < input.Length; i++)
-                {
-                    CRC = ((CRC & 0xFFFFFF00) / 0x100) & 0xFFFFFF ^ pTable[input[i] ^ CRC & 0xFF];
-                }
-                CRC = (-(CRC)) - 1; // !(CRC)
-
-                return (uint)CRC;
-            }
-
-        }
+        
 
         #region Fields
         readonly BinaryWriter _writer;
@@ -244,67 +195,7 @@ namespace CMK
             }
         }
 
-        private byte[] getSwappedCrc(byte[] chunk)
-        {
-            var crc32 = new CrcCalculator();
-            var crc = crc32.GetCRC32(chunk);
-            var crcArray = BitConverter.GetBytes(crc);
-            Array.Reverse(crcArray);
-            return crcArray;
-        }
 
-        private byte[] getSwappedArray(int i)
-        {
-            Byte[] bytes = BitConverter.GetBytes(i);
-            Array.Reverse(bytes);
-            return bytes;
-        }
-
-        private byte[] getSwappedArray(short s)
-        {
-            Byte[] bytes = BitConverter.GetBytes(s);
-            Array.Reverse(bytes);
-            return bytes;
-        }
-
-        private List<Byte[]> find_IHDR(Stream png)
-        {
-            return find(png, "IHDR".ToCharArray());
-        }
-
-        private List<Byte[]> find_IDAT(Stream png)
-        {
-            return find(png, "IDAT".ToCharArray());
-        }
-
-        private List<Byte[]> find(Stream png, Char[] search)
-        {
-            List<Byte[]> result = new List<byte[]>();
-            var searchBytes = search.Select(c => (byte)c).ToArray();
-            Byte[] bytes = new Byte[search.Length];
-            int i = 0;
-            int found = 0;
-            while (i < png.Length - 4)
-            {
-                png.Flush();
-                png.Position = i;
-                var debug = png.Read(bytes, 0, search.Length);
-                i++;
-                if (bytes.SequenceEqual(searchBytes))
-                {
-                    Byte[] rawLength = new Byte[4];
-                    png.Position -= 8;
-                    png.Read(rawLength, 0, 4);
-                    Array.Reverse(rawLength);
-                    UInt32 length = BitConverter.ToUInt32(rawLength, 0);
-                    result.Add(new Byte[length + 12]);
-                    png.Position -= 4;
-                    png.Read(result[found], 0, (int)(length + 12));
-                    found++;
-                }
-            }
-            return result;
-        }
 
         private void write(Byte[] data)
         {
